40ffb59c1bf3e7c0d7852511ebf92aa3e4fc9e26
nagaham <nagahama2015@gmail.com>
2022-01-14 14:11:46 +0900

    Initial commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.testapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
+import android.view.View
+
+import android.widget.TextView
+import com.example.testapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.tv01.text = "😱"
+        movemove()
+
+    }
+    fun movemove(){
+        var moving = TranslateAnimation(-200f,200f,0f,0f)
+        moving.duration = 2000// 20ミリ秒)かけて行う
+        binding.tv01.animation = moving // ビューにアニメーションを適用する
+        moving.repeatMode= Animation.REVERSE
+        moving.repeatCount = 50
+    }
+}
